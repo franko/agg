cd4beeab70cc9e9be3f1c5871749ce4e5049a157
Francesco Abbate <francesco.bbt@gmail.com>
2020-05-22 18:23:25 +0200

    Implement very basic library for LCD font rendering using AGG

---
+++ research/truetype_lcd/font-render-test.cpp
@@ -0,0 +1,55 @@
+#include <new>
+#include <stdio.h>
+
+#include "font_render_lcd.h"
+
+#if defined(_WIN32) || defined(WIN32)
+static const char *os_font_directory = "C:/Windows/fonts";
+#else
+static const char *os_font_directory = "/usr/share/fonts/truetype";
+#endif
+
+static char *path_join(const char *dir, const char *filename) {
+    const int path_len = strlen(dir), filename_len = strlen(filename);
+    const int len = path_len + 1 + filename_len + 1;
+    char *full_name = new char[len];
+    memcpy(full_name, dir, path_len);
+    memcpy(full_name + path_len, "/", 1);
+    memcpy(full_name + path_len + 1, filename, filename_len);
+    full_name[len - 1] = 0;
+    return full_name;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc > 2) {
+        fprintf(stderr, "usage: %s [truetype-font-dir]\n", argv[0]);
+        return 1;
+    }
+
+    if (argc == 2) {
+        os_font_directory = argv[1];
+    }
+    fprintf(stderr, "looking for truetype fonts in directory %s.\n", os_font_directory);
+
+    const char *font_filename = "liberation/LiberationSerif-Regular.ttf";
+    const char *font_full_path = path_join(os_font_directory, font_filename);
+
+    agg::rendering_buffer ren_buf;
+    const unsigned width = 640, height = 640;
+    const unsigned pixel_size = 3;
+    agg::int8u *buffer_data = new(std::nothrow) agg::int8u[width * height * pixel_size];
+    if (!buffer_data) {
+        fprintf(stderr, "Cannor allocate memory for buffer.\n");
+        return 1;
+    }
+    ren_buf.attach(buffer_data, width, height, -width * pixel_size);
+
+    font_renderer_lcd font_renderer;
+    font_renderer.clear(ren_buf, agg::rgba8(0xe9, 0xe5, 0xcd));
+
+    font_renderer.render_text(ren_buf, font_full_path,
+        12, agg::rgba8(0x23, 0x1f, 0x20), 320, 320, "Hello world!");
+
+    return 0;
+}
--- research/truetype_lcd/freetype_lcd_sdl.cpp
+++ research/truetype_lcd/freetype_lcd_sdl.cpp
@@ -1,62 +1,63 @@
-// This example shows how to create a SDL_Surface* with the data loaded from an image
-// file with the stb_image.h library (https://github.com/nothings/stb/)
+#include <stdio.h>
 
-// the color format you request stb_image to output,
-// use STBI_rgb if you don't want/need the alpha channel
-#include "SDL_surface.h"
-#include "SDL_log.h"
-#include "stb_image.h"
+#include "SDL.h"
 
-int main() {
-  int req_format = STBI_rgb_alpha;
-  int width, height, orig_format;
-  unsigned char* data = stbi_load("./test.png", &width, &height, &orig_format, req_format);
-  if(data == NULL) {
-    SDL_Log("Loading image failed: %s", stbi_failure_reason());
-    exit(1);
-  }
+int main(int argc, char *argv[])
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+    SDL_Event event;
 
-  // Set up the pixel format color masks for RGB(A) byte arrays.
-  // Only STBI_rgb (3) and STBI_rgb_alpha (4) are supported here!
-  Uint32 rmask, gmask, bmask, amask;
-  #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    int shift = (req_format == STBI_rgb) ? 8 : 0;
-    rmask = 0xff000000 >> shift;
-    gmask = 0x00ff0000 >> shift;
-    bmask = 0x0000ff00 >> shift;
-    amask = 0x000000ff >> shift;
-  #else // little endian, like x86
-    rmask = 0x000000ff;
-    gmask = 0x0000ff00;
-    bmask = 0x00ff0000;
-    amask = (req_format == STBI_rgb) ? 0 : 0xff000000;
-  #endif
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
 
-  int depth, pitch;
-  if (req_format == STBI_rgb) {
-    depth = 24;
-    pitch = 3*width; // 3 bytes per pixel * pixels per row
-  } else { // STBI_rgb_alpha (RGBA)
-    depth = 32;
-    pitch = 4*width;
-  }
+    if (SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
+    }
 
-  SDL_Surface* surf = SDL_CreateRGBSurfaceFrom((void*)data, width, height, depth, pitch,
-                                               rmask, gmask, bmask, amask);
+    surface = SDL_LoadBMP("sample.bmp");
+    if (!surface) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
+        return 3;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!texture) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
+        return 3;
+    }
+    SDL_FreeSurface(surface);
 
-  if (surf == NULL) {
-    SDL_Log("Creating surface failed: %s", SDL_GetError());
-    stbi_image_free(data);
-    exit(1);
-  }
+    while (1) {
+        if (SDL_WaitEvent(&event) == 0) {
+            continue;
+        }
+        if (event.type == SDL_QUIT) {
+            break;
+        }
+        if (event.type == SDL_WINDOWEVENT) {
+            Uint8 wet = event.window.event;
+            if (wet >= SDL_WINDOWEVENT_SHOWN && wet <= SDL_WINDOWEVENT_SIZE_CHANGED) {
+                fprintf(stdout, "render event type %x window event type: %d\n",
+                    event.type,
+                    event.window.event);
+                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);
+            }
+        }
+    }
 
-  // ... do something useful with the surface ...
-  // ...
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 
-  // when you don't need the surface anymore, free it..
-  SDL_FreeSurface(surf);
-  // .. *and* the data used by the surface!
-  stbi_image_free(data);
+    SDL_Quit();
 
-  return 0;
+    return 0;
 }
